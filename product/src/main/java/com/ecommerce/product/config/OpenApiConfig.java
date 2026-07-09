bc6ed320eded6859360fb9de9c88a937b5c46aca
MiguelosAguialos <aguiarmiguel65@gmail.com>
2026-07-09 15:34:24 +0200

    feat: product microservice setup

--- product/src/main/java/com/ecommerce/product/config/OpenApiConfig.java
+++ product/src/main/java/com/ecommerce/product/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.ecommerce.user.config;
+package com.ecommerce.product.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -9,16 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Microservices E-commerce API - User MS",
+                title = "Microservices E-commerce API - Porduct MS",
                 version = "0.0.1",
-                description = "API REST (microservice) of a simple e-commerce to handle users.",
+                description = "API REST (microservice) of a simple e-commerce to handle products.",
                 contact = @Contact(
                         name = "MFTech",
                         email = "support@mftech.com"
                 )
         ),
         servers = {
-                @Server(url = "http://localhost:8082", description = "Local environment - USER MS")
+                @Server(url = "http://localhost:8081", description = "Local environment - PRODUCT MS")
         }
 )
 public class OpenApiConfig {
